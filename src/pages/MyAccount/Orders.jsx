2a26e887ac10e577caa25fd1ca939daeb39f89ae
Harshad Pagare <harshadpagare@gmail.com>
2026-04-30 14:18:51 +0530

    my Order page, fetch issue solved

--- src/pages/MyAccount/Orders.jsx
+++ src/pages/MyAccount/Orders.jsx
@@ -51,7 +51,7 @@ const Orders = () => {
         setLoading(true);
 
         const res = await axios.get(
-          `http://localhost:4000/api/orders/`,
+          `${import.meta.env.VITE_BASE_PORT}/api/orders`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
